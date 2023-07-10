6c3096113f4cb94110f0eb1bdcdf447263e32aef
SrVertex <121757774+SrVertex@users.noreply.github.com>
2023-07-10 16:49:13 -0300

    bb

---
+++ app/metodoMap.js
@@ -0,0 +1,14 @@
+function aplicaDesconto(livros) {
+    const desconto = 0.3;
+    livrosComDesconto = livros.map (livro => {
+
+
+        return {...livro, preco: livro.preco - (livro.preco * desconto)};
+
+
+
+
+    })
+
+    return livrosComDesconto
+}
