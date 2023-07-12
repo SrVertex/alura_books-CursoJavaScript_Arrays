b12d6fd4226c75a83f522dbf37db111b9df23d2d
SrVertex <121757774+SrVertex@users.noreply.github.com>
2023-07-12 11:29:22 -0300

    FINALIZADO_TREINAMENTO

--- app/main.js
+++ app/main.js
@@ -3,12 +3,9 @@ const endpointAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.json
 
 getBuscarLivrosAPI();
 
-
-
 async function getBuscarLivrosAPI() {
     const res = await fetch(endpointAPI); 
     livros = await res.json();
     let livrosComDesconto = aplicaDesconto(livros);
     exibirOsLivrosNaTela(livrosComDesconto);
-}
-
+} 
--- app/metodoForIt.js
+++ app/metodoForIt.js
@@ -1,10 +1,15 @@
 const elementoParaInserirLivro = document.getElementById("livros");
+const elementoValorTotalLivrosDisponiveis = document.getElementById("valor_total_livros_disponiveis")
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoValorTotalLivrosDisponiveis.innerHTML = "";
+    elementoParaInserirLivro.innerHTML = ""
     listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponibilidadeLivro(livro);
+        let disponibilidade = livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
         elementoParaInserirLivro.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
         <h2 class="livro__titulo">
           ${livro.titulo}
         </h2>
@@ -15,5 +20,21 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         </div>
       </div>`
     });
-
 }
+
+function verificarDisponibilidadeLivro(livro) {
+    if (livro.quantidade > 0) {
+        return "livro__imagens";
+    } else {
+        return "livro__imagens indisponivel";
+    }
+}
+
+// function verificarDisponibilidadeLivro(livro){
+//   if (livro.quantidade > 0) {
+//     return "livro__imagens";
+//   } else{
+//     return "livro__imagens indisponivel";
+//   }
+
+// }
